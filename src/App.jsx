eba5c315d232b74265bab53e0848d4bdfe22701c
AnuragShukla1999 <shuklaanurag310@gmail.com>
2023-10-25 14:32:17 +0530

    Add AboutUs Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer'
 import HomePage from './Pages/HomePage'
+import AboutUs from './Pages/AboutUs'
 
 const App = () => {
   return (
     <>
       <Routes>
              <Route path="/" element={<HomePage/>} />
+             <Route path="/about" element={<AboutUs/>} />
       </Routes>
       {/* <Footer/> */}
     </>
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -44,75 +44,74 @@ function HomeLayout({ children }) {
 
 
     return (
-        <div className="min-h-[90vh] ">
-            <div className="drawer relative left-0 bottom-0 z-50 w-fit">
-                <input className="drawer-toggle" type="checkbox" id="my-drawer" />
+        <div className="min-h-[90vh]">
+            <div className="drawer absolute left-0 z-50 w-full">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
-                    <label htmlFor="my-drawer" className="cursor-pointer relative">
+                    <label htmlFor="my-drawer">
                         <FiMenu onClick={changeWidth} size={"32px"} className='font-bold text-white m-4' />
                     </label>
                 </div>
-
-                <div className='drawer-side '>
-                    <label htmlFor="my-drawer" className='drawer-overplay'>
-
-                    </label>
-                    <ul className="menu p-4 w-48 sm:w-80 h-60 bg-base-300 text-base-content relative">
+                <div className="drawer-side w-0">
+                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
+                    <ul className='menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative'>
                         <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
-                                <AiFillCloseCircle size={24} />
+                                <AiFillCloseCircle size={24}/>
                             </button>
                         </li>
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/"> Home </Link>
                         </li>
-
-
-                        {isLoggedIn && role === 'ADMIN' && (
+                        {isLoggedIn && role === "ADMIN" && (
                             <li>
-                                <Link to='/admin/dashboard'> Admin Dashboard </Link>
+                                <Link to="/admin/dashboard">Admin Dashboard</Link>
                             </li>
-                        )}
+                        )
 
+                        }
+                        {isLoggedIn && role === "ADMIN" && (
+                            <li>
+                                <Link to="/course/create">Create Course</Link>
+                            </li>
+                        )
 
+                        }
                         <li>
-                            <Link to="/courses">All Courses</Link>
+                            <Link to="/about"> About us </Link>
                         </li>
                         <li>
-                            <Link to="/contact">Contact Us</Link>
+                            <Link to="/contact"> Contact us </Link>
                         </li>
                         <li>
-                            <Link to="/about">About Us</Link>
+                            <Link to="/courses"> All courses </Link>
                         </li>
 
-
-
-                        {!isLoggedIn && (
-                            <li className='absolute bottom-4 w-[90%]'>
-                                <div className='w-full flex items-center justify-center'>
-                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md'>
-                                        <Link to='/login'>Login</Link>
+                        {!isLoggedIn ? (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/signin">Login</Link>
                                     </button>
-                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md'>
-                                        <Link to='/login'>Signup</Link>
+                                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/signup">Signup</Link>
                                     </button>
                                 </div>
                             </li>
-                        )}
-
-
-                        {isLoggedIn && (
-                            <li className='absolute bottom-4 w-[90%]'>
-                                <div className='w-full flex items-center justify-center'>
-                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md'>
-                                        <Link to='/user/profile'>Profile</Link>
+                        ) : (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/user/profile">Profile</Link>
                                     </button>
-                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md'>
-                                        <Link onClick={handleLogout}>Logout</Link>
+                                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link onClick={onLogout}>Logout</Link>
                                     </button>
                                 </div>
                             </li>
-                        )}
+                        )
+
+                        }
                     </ul>
                 </div>
             </div>
@@ -120,6 +119,7 @@ function HomeLayout({ children }) {
             {children}
 
             <Footer />
+
         </div>
     )
 
